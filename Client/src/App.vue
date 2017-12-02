aa5c731a0224f75d5019bb78fd19229769555be9
devin.kiser <devin.kiser@asynchrony.com>
2017-12-02 00:01:07 -0600

    Initialize vue project. Devin

---
+++ Client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import Background from './components/Background';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
