8303e2d11b9b27c09fce9596c60cde12e08755fd
Chip Thien <chip@example.com>
2017-12-01 19:59:44 -0600

    Commit notes and contract interface.

---
+++ BountyContract/contract.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.19;
+
+contract BlockBounty {
+
+  address public owner;
+  mapping (address => uint) public contributions;
+
+  modifier isOwner() {
+      require(msg.sender == owner);
+      _;
+  }
+
+  function BlockBounty() public {
+    owner = msg.sender;
+  }
+
+  function createJob(uint totalWorkRequired) isOwner() public {
+
+  }
+
+  function contribute(address contributor, uint numberOfWorks) isOwner() public {
+
+  }
+}
