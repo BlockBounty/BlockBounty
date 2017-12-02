b32c5610c2dd690e6a336ba67cc7beed60580329
Chip Thien <chip@example.com>
2017-12-02 00:07:22 -0600

    First crack at paying everyone for contributing.

--- BountyContract/contracts/BlockBounty.sol
+++ BountyContract/contracts/BlockBounty.sol
@@ -3,8 +3,18 @@ pragma solidity ^0.4.18;
 contract BlockBounty {
 
   uint public totalWorkRequired;
+  uint public totalJobPayout;
+  uint public workDoneSoFar;
   address public owner;
-  mapping (address => uint) public contributions;
+  mapping (address => Contribution) public contributions;
+  address[] public contributors;
+
+  struct Contribution {
+    uint contributions;
+    bool isContributor;
+  }
+
+  //TOOD: modifier to prevent anything from happening once the job is done
 
   modifier isOwner() {
       require(msg.sender == owner);
@@ -15,11 +25,26 @@ contract BlockBounty {
     owner = msg.sender;
   }
 
-  function createJob(uint _totalWorkRequired) isOwner() public {
+  function createJob(uint _totalWorkRequired, uint _totalJobPayout) isOwner() payable public {
     totalWorkRequired = _totalWorkRequired;
+    totalJobPayout = _totalJobPayout;
   }
 
   function contribute(address contributor, uint numberOfWorks) isOwner() public {
-    contributions[contributor] += numberOfWorks;
+    if (!contributions[contributor].isContributor) {
+      contributors.push(contributor);
+    }
+    contributions[contributor].contributions += numberOfWorks;
+    workDoneSoFar += numberOfWorks;
+    if (workDoneSoFar >= totalWorkRequired) {
+      payEveryone();
+    }
+  }
+
+  function payEveryone() private {
+    for (uint i = 0; i < contributors.length; i++) {
+      address contributor = contributors[i];
+      contributor.transfer(totalJobPayout * contributions[contributor].contributions / totalWorkRequired); //WARN: integer math leads to errors
+    }
   }
 }
