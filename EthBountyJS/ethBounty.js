68f6ee09e9efa0964c5307f0d7de6ca9ce4cd9dc
Chip Thien <chip@example.com>
2017-12-01 21:53:17 -0600

    Working install of truffle. Has migrations and deploys contract.

--- EthBountyJS/ethBounty.js
+++ EthBountyJS/ethBounty.js
@@ -1,18 +1,18 @@
 const contract = require('truffle-contract');
 const abi = require('../BountyContract/build/contracts/BlockBounty.json');
 const Web3 = require('web3');
-const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
+const myweb3 = new Web3(provider);
 
 let newBounty = () => {
     let newContract = contract(abi);
-
-    newContract.setProvider(provider);
+    newContract.setProvider(myweb3.currentProvider);
     console.log("Pre-new");
     newContract.deployed().then(res => {
-        console.log(res);
+        console.log("win");
     }).catch(error => {
         console.log(error);
-    });;
+    });
 
 };
 
